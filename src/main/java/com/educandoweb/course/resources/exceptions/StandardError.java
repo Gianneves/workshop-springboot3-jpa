51e2393175ae4c5d1057549913fdc84310347120
Gian Neves <gianneves@hotmail.com>
2023-10-09 18:40:29 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -1,5 +1,7 @@
 package com.educandoweb.course.resources.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
@@ -8,6 +10,7 @@ public class StandardError implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
     private Integer status;
     private String error;
